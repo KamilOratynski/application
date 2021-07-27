ea161e61e83cdb64e9b033ab1f0b6de9746d90c6
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-07-27 22:42:52 +0200

    Added character selection

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Application {
 
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("xxxx");
+        System.out.print("Choose a char: ");
+
+        String s = sc.nextLine();
+        char ch = s.charAt(0);
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (ch == c) {
+                for (int i = 0; i < 4; i++) {
+                    System.out.print(c);
+                }
+            }
+        }
     }
-    
 }
