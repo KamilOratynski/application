fa5148d56ab1af4b4587b96c98382d91bf0d7e3d
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-07-27 23:13:35 +0200

    Added possibility choose only one letter

--- src/Application.java
+++ src/Application.java
@@ -5,17 +5,24 @@ public class Application {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Choose a char: ");
+        System.out.print("Choose a letter: ");
 
         String s = sc.nextLine();
         char ch = s.charAt(0);
 
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (ch == c) {
-                for (int i = 0; i < 4; i++) {
-                    System.out.print(c);
+        boolean isLetter = Character.isLetter(ch);
+        char[] b = s.toCharArray();
+
+        if (isLetter && b.length == 1) {
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (ch == c) {
+                    for (int i = 0; i < 4; i++) {
+                        System.out.print(c);
+                    }
                 }
             }
+        } else {
+            System.out.println("Selected character isn't a letter or is more than one. Choose only one letter!");
         }
     }
 }
