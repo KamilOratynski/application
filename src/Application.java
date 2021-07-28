d69fc9a45929d48ec0d951fd80823dd5551e5b53
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-07-28 20:54:45 +0200

    Revert "Added possibility choose only one letter" This reverts commit fa5148d5

--- src/Application.java
+++ src/Application.java
@@ -5,24 +5,17 @@ public class Application {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Choose a letter: ");
+        System.out.print("Choose a char: ");
 
         String s = sc.nextLine();
         char ch = s.charAt(0);
 
-        boolean isLetter = Character.isLetter(ch);
-        char[] b = s.toCharArray();
-
-        if (isLetter && b.length == 1) {
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (ch == c) {
-                    for (int i = 0; i < 4; i++) {
-                        System.out.print(c);
-                    }
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (ch == c) {
+                for (int i = 0; i < 4; i++) {
+                    System.out.print(c);
                 }
             }
-        } else {
-            System.out.println("Selected character isn't a letter or is more than one. Choose only one letter!");
         }
     }
 }
