1ec0fff027905378c910fdd6f0f987ad7bd632d9
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-08-01 21:28:33 +0200

    Refactor to preparation unit tests

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,12 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("xxxx");
+        Application app = new Application();
+        System.out.println(app.print());
     }
-    
+
+    public String print() {
+        return "xxxx";
+    }
+
 }
