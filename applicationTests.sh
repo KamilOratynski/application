d27b017c8d9ae5609706313b5aca0f7553111e40
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-08-17 20:26:47 +0200

    Refactored test and added expected file

--- applicationTests.sh
+++ applicationTests.sh
@@ -1,5 +1,3 @@
-echo "xxxx" > expected.txt
-
 java Application.java > output.txt
 
-diff -b --color expected.txt output.txt && rm expected.txt output.txt
+diff -u --color ./output.txt ./expected.txt && rm output.txt
