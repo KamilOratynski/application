894954fff4ca48bc7f0b210b1d447671d256482b
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-08-19 18:48:47 +0200

    Added possibility of entering quantity of characters

--- Application.java
+++ Application.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("xxxx");
+        System.out.println("Choose number of characters:");
+        Scanner scanner = new Scanner(System.in);
+        int j = scanner.nextInt();
+        for (int i = 0; i < j; i++) {
+            System.out.print("x");
+        }
     }
-    
+
 }
