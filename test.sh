2c9d6f175dcdf422ad4f449b116c73128c25e686
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-09-20 18:25:49 +0200

    Added more tests

--- test.sh
+++ test.sh
@@ -4,4 +4,8 @@ set -e
 
 java Application | diff -u --color - ./default.txt
 
-java Application 5 | diff -u --color - ./5.txt
+java Application 0 | diff -u --color - ./0.txt
+
+java Application 1 | diff -u --color - ./1.txt
+
+java Application 2 | diff -u --color - ./2.txt
