97a0cc62ce48a1d517cab5e9337e8931895745b3
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-09-20 17:48:56 +0200

    User can define char count

--- Application.java
+++ Application.java
@@ -1,7 +1,14 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("xxxx");
+        if (args.length != 0) {
+            int j = Integer.parseInt(args[0]);
+            for (int i = 0; i < j; i++) {
+                System.out.print("x");
+            }
+        } else {
+            System.out.print("xxxx");
+        }
+        System.out.println();
     }
-    
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
+set -e
+
 ./release.sh
 
-java Application | diff -u --color - ./expected.txt
+java Application | diff -u --color - ./withoutParameter.txt
+
+java Application 5 | diff -u --color - ./fiveChars.txt
