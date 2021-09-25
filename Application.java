d708244cba84432112af81507dfe9a8ff71dbedc
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-09-25 11:12:29 +0200

    Refactored and added unit tests

--- Application.java
+++ Application.java
@@ -1,13 +1,45 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Application {
 
     public static void main(String[] args) {
+        String out = run(args);
+        System.out.print(out);
+    }
+
+    private static String run(String[] args) {
         int count = 4;
         if (args.length > 0) {
             count = Integer.parseInt(args[0]);
         }
+
+        String out = "";
         for (int i = 0; i < count; i++) {
-            System.out.print("x");
+            out += "x";
         }
-        System.out.println();
+        out += "\n";
+        return out;
+    }
+
+    @Test
+    public void test() {
+        String args[] = {"0"};
+        String out = Application.run(args);
+        Assertions.assertEquals("\n", out);
+    }
+
+    @Test
+    public void test2() {
+        String args[] = {"1"};
+        String out = Application.run(args);
+        Assertions.assertEquals("x\n", out);
+    }
+
+    @Test
+    public void test3() {
+        String args[] = {"2"};
+        String out = Application.run(args);
+        Assertions.assertEquals("xx\n", out);
     }
 }
