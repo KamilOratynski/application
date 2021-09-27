5c7f3c60c04c638b6d9d1cdaff164cadf92d3cf2
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-09-27 16:33:17 +0200

    Java style array declaration

--- Application.java
+++ Application.java
@@ -24,21 +24,21 @@ public class Application {
 
     @Test
     public void withParam0() {
-        String args[] = {"0"};
+        String[] args = {"0"};
         String out = Application.run(args);
         Assertions.assertEquals("\n", out);
     }
 
     @Test
     public void withParam1() {
-        String args[] = {"1"};
+        String[] args = {"1"};
         String out = Application.run(args);
         Assertions.assertEquals("x\n", out);
     }
 
     @Test
     public void withParam2() {
-        String args[] = {"2"};
+        String[] args = {"2"};
         String out = Application.run(args);
         Assertions.assertEquals("xx\n", out);
     }
