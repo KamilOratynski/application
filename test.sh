7faad0f346d9daf87d195f55f38c438cdf70ccba
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-10-12 19:16:51 +0200

    Added one test to test.sh - check replaced char

--- test.sh
+++ test.sh
@@ -17,3 +17,5 @@ java Application 0 z | diff -Zu --color - ./0.txt
 java Application 1 z | diff -Zu --color - ./1z.txt
 
 java Application 2 z | diff -Zu --color - ./2z.txt
+
+echo 1a | java Application 2 y | diff -Zu --color - ./2yReplace1a.txt
