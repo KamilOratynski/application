cf08420cd6d53488678591d66847198070ecfb32
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-11-11 18:59:34 +0100

    Test for usage message check only prefix

--- test.sh
+++ test.sh
@@ -4,11 +4,7 @@ javac -cp junit-platform-console-standalone-1.8.1.jar Application.java
 
 java -jar ./junit-platform-console-standalone-1.8.1.jar --disable-banner --details=none -cp . -c Application
 
-java Application | diff -Zu --color - <(cat <<<"Usage: app number [character]
-
-Arguments:
-number    - set number of characters displaying
-character - set character to display, 'x' is displayed by default")
+java Application | grep '^Usage: .\+' > /dev/null
 
 echo q | java Application 0 | diff -Zu --color - <(cat <<<'')
 
@@ -56,3 +52,5 @@ yyyyyyyyya
 qyyyyyyyya
 qwyyyyyyya
 qwyyyeyyya')
+
+echo OK
