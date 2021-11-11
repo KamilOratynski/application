b00d66a711292b41ce12bab1b8dba21c63a15f26
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-11-11 18:09:33 +0100

    Moved help do test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,12 @@ javac -cp junit-platform-console-standalone-1.8.1.jar Application.java
 
 java -jar ./junit-platform-console-standalone-1.8.1.jar --disable-banner --details=none -cp . -c Application
 
-java Application | diff -Zu --color - ./help.txt
+java Application | diff -Zu --color - <(cat <<<"java Application: missing parameter
+Usage: java Application <number> [character]
+
+Arguments:
+<number>    - set number of characters displaying
+[character] - set character to display, 'x' is displayed by default")
 
 echo q | java Application 0 | diff -Zu --color - <(cat <<<'')
 
