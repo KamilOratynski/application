3e73a3cfa21bf5aef221797c3d210728a2d1b8ba
Kamil Oratyński <kamil.oratynski@gmail.com>
2021-11-23 11:18:57 +0100

    Adapted simple tests to v5

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-set -eo pipefail
+set -exo pipefail
 
 javac -cp junit-platform-console-standalone-1.8.1.jar Application.java
 
@@ -10,26 +10,42 @@ echo q | java Application 0 | diff -Zu --color - <(cat <<<'')
 
 echo q | java Application 1 | diff -Zu --color - <(cat <<<'x')
 
-echo q | java Application 2 | diff -Zu --color - <(cat <<<'xxxx')
+echo q | java Application 2 | diff -Zu --color - <(cat <<<'xx
+xx')
 
 echo q | java Application 0 z | diff -Zu --color - <(cat <<<'')
 
 echo q | java Application 1 z | diff -Zu --color - <(cat <<<'z')
 
-echo q | java Application 2 z | diff -Zu --color - <(cat <<<'zzzz')
+echo q | java Application 2 z | diff -Zu --color - <(cat <<<'zz
+zz')
 
 echo 1a q | java Application 1 y | diff -Zu --color - <(cat <<<'y
 a')
 
-echo 1a q | java Application 2 y | diff -Zu --color - <(cat <<<'yyyy
-ayyy')
+echo 1a q | java Application 2 y | diff -Zu --color - <(cat <<<'yy
+yy
+ay
+yy')
 
-echo 2a q | java Application 2 y | diff -Zu --color - <(cat <<<'yyyy
-yayy')
+echo 2a q | java Application 2 y | diff -Zu --color - <(cat <<<'yy
+yy
+ya
+yy')
 
-echo 1x 2y q | java Application 2 z | diff -Zu --color - <(cat <<<'zzzz
-xzzz
-xyzz')
+echo 1x 2y q | java Application 2 z | diff -Zu --color - <(cat <<<'zz
+zz
+xz
+zz
+xy
+zz')
+
+echo 3x 4y q | java Application 2 z | diff -Zu --color - <(cat <<<'zz
+zz
+zz
+xz
+zz
+xy')
 
 echo 1x 2y 3a 1q 6w 9e q | java Application 3 z | diff -Zu --color - <(cat <<<'zzzzzzzzz
 xzzzzzzzz
@@ -38,10 +54,6 @@ xyazzzzzz
 qyazzzzzz
 qyazzwzzz
 qyazzwzze')
-
-echo 3x 4y q | java Application 2 z | diff -Zu --color - <(cat <<<'zzzz
-zzxz
-zzxy')
 
 echo 9x 8y 7a q | java Application 3 z | diff -Zu --color - <(cat <<<'zzzzzzzzz
 zzzzzzzzx
